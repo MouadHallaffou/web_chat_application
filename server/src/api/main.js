3aa6a5ea23d2f2a06f89b6d53e3ab0b6d617b06d
MouadHallaffou <mouadhallaffou@gmail.com>
2025-05-25 16:04:15 +0100

    fixe strecture

--- server/src/api/main.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');   
-const { getAllUsers, createUser, updateUser, deleteUser } = require('./controllers/userController');
-const { getAllPosts, createPost, updatePost, deletePost } = require('./controllers/postController');
